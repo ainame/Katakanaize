1a2d6b7a69a4841b9b0e4d8caa707e0d9bd9e0ee
Satoshi Namai <ainame954@facebook.com>
2012-04-29 12:00:21 +0900

    support some encodings (utf8, euc-jp, shift_jis, iso-2022-jis)

--- lib/katakanaize.rb
+++ lib/katakanaize.rb
@@ -4,13 +4,29 @@ require 'rack/response'
 require 'nkf'
 
 class Katakanaize
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
     status, headers, response = @app.call(env)
-    new_body = [NKF.nkf('-wh2', response[0])]
-    Rack::Response.new(new_body, status, headers).finish
+    katakanaized_body = response.map{ |body| katakanaize(body) }
+    Rack::Response.new(katakanaized_body, status, headers).finish
   end
+  
+  private
+  def katakanaize(str)
+    case NKF.guess(str).to_s
+    when "UTF-8"
+      NKF.nkf('-wh2', str)
+    when "EUC-JP"
+      NKF.nkf('-eh2', str)
+    when "Shift_JIS"
+      NKF.nkf('-sh2', str)
+    when "ISO-2022-JP"
+      NKF.nkf('-jh2', str)
+    end
+  end
+
 end
